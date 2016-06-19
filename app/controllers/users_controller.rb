57d2ccd3a88f7d776cd7efb6c38d2d0ce4a03622
Ena Bek <enabek@gmail.com>
2016-06-19 19:10:08 -0400

    Add complete user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,21 @@ class UsersController < ApplicationController
     @articles = @user.articles
     @comment = @user.comments
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:id] = @user.id
+      redirect_to @user
+    else
+      @errors = @user.errors.full_messages
+      render 'new'
+    end
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
