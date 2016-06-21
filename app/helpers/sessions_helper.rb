5d6601c8988d753e4174e8af4450b1d389131bc3
Ena Bek <enabek@gmail.com>
2016-06-21 19:27:53 -0400

    Add session helpers, navbar datetime parse

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+  def log_in(user)
+    session[:id] = user.id
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:id])
+  end
 end
