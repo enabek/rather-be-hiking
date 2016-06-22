df262bea62da6d240760764655b77fd771e278c2
Ena Bek <enabek@gmail.com>
2016-06-21 20:39:11 -0400

    Add comment controller views for new comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+    @article = Article.find_by(id: [:article_id])
+    @comment.user_id = current_user.id
+    @comment.article_id = @article.id
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @article = Article.find(params[:article_id])
+    @comment.user_id = current_user.id
+    @comment.article_id = @article.id
+    if @comment.save
+      render
+    else
+      redirect
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
